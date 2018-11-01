6ee4e85b0150a11fad9d9c5fad994b9c18e40777
balajibitta <bittabalaji@gmail.com>
2018-11-01 14:50:50 +0530

    Added PrintTest class

---
+++ src/com/OopsConcepts/java/PrintTest.java
@@ -0,0 +1,16 @@
+package com.OopsConcepts.java;
+
+public class PrintTest {
+
+	public static String name = "balaji";
+	public static String id = "balu123";
+	
+	public static void main(String[] args) {
+
+		System.out.println(name);
+		System.out.println(id);
+
+		
+	}
+
+}
